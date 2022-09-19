2b65291ac408d51c93074c618866521e055b57da
Alex Fernandez <alexfernandez@Sergios-MacBook-Pro.local>
2022-09-19 21:43:53 +0200

    actualizar clase Main con métodos

--- src/n2exercici1/Main.java
+++ src/n2exercici1/Main.java
@@ -20,46 +20,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		do {
-			System.out.println("Indica que quiere hacer :\n"
-							+ "1.Añadir telefono.\n"
-							+ "2.Añadir direccion.\n"
-							+ "0.Salir.");
+			System.out.println(
+					"Indica que quiere hacer :\n" + "1.Añadir telefono.\n" + "2.Añadir direccion.\n" + "0.Salir.");
 			opcion = entrada.nextInt();
 			entrada.nextLine();
 			switch (opcion) {
 			case 1:
-				System.out.println("Introduzca el numero de telefono :");
-				numTelefono = entrada.nextLine();
-				fabricaCrear = "Telefono";
-				fabrica = Fabrica.crearFabrica(fabricaCrear);
-				telefonoCreado = fabrica.crearTelefono(numTelefono);
-				if (telefonoCreado == null) {
-					salidaPantalla = "El numero no coincide con ningun pais registrado.";
-				} else {
-					agenda.añadir(telefonoCreado);
-					salidaPantalla = "Telefono añadido correctamente.";
-				}
+				añadirTelefono();
 				break;
 			case 2:
-				System.out.println("Introduzca destinatario :");
-				destinatario = entrada.nextLine();
-				System.out.println("Introduzca calle/avenida/via :");
-				calle = entrada.nextLine();
-				System.out.println("Introduzca codigo postal :");
-				codigoPostal = entrada.nextLine();
-				System.out.println("Introduzca pais :");
-				pais = entrada.nextLine();
-				fabricaCrear = "Direccion";
-				fabrica = Fabrica.crearFabrica(fabricaCrear);
-				direccionCreada = fabrica.crearDireccion(destinatario,calle,codigoPostal,pais);
-				if (direccionCreada == null) {
-					salidaPantalla = "La direccion no coincide con ningun pais registrado.";
-				} else {
-					agenda.añadir(direccionCreada);
-					salidaPantalla = "Direccio añadida correctamente.";
-				}
+				añadirDireccion();
 				break;
 			case 0:
 				salidaPantalla = "Bye,bye.";
@@ -70,5 +42,39 @@ public class Main {
 			}
 			System.out.println(salidaPantalla);
 		} while (opcion != 0);
+	}
+
+	public static void añadirTelefono() {
+		System.out.println("Introduzca el numero de telefono :");
+		numTelefono = entrada.nextLine();
+		fabricaCrear = "Telefono";
+		fabrica = Fabrica.crearFabrica(fabricaCrear);
+		telefonoCreado = fabrica.crearTelefono(numTelefono);
+		if (telefonoCreado == null) {
+			salidaPantalla = "El numero no coincide con ningun pais registrado.";
+		} else {
+			agenda.añadir(telefonoCreado);
+			salidaPantalla = "Telefono añadido correctamente.";
+		}
+	}
+
+	public static void añadirDireccion() {
+		System.out.println("Introduzca destinatario :");
+		destinatario = entrada.nextLine();
+		System.out.println("Introduzca calle/avenida/via :");
+		calle = entrada.nextLine();
+		System.out.println("Introduzca codigo postal :");
+		codigoPostal = entrada.nextLine();
+		System.out.println("Introduzca pais :");
+		pais = entrada.nextLine();
+		fabricaCrear = "Direccion";
+		fabrica = Fabrica.crearFabrica(fabricaCrear);
+		direccionCreada = fabrica.crearDireccion(destinatario, calle, codigoPostal, pais);
+		if (direccionCreada == null) {
+			salidaPantalla = "La direccion no coincide con ningun pais registrado.";
+		} else {
+			agenda.añadir(direccionCreada);
+			salidaPantalla = "Direccio añadida correctamente.";
+		}
 	}
 }
